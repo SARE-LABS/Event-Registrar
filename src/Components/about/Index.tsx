591e5db8f2e8e133a34b46473a41aebbb319e7e0
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-08 22:52:51 +0100

    feat: update About and Footer components with improved layout and social media links

--- src/Components/about/Index.tsx
+++ src/Components/about/Index.tsx
@@ -4,7 +4,7 @@ import { Tags } from "./Tags"
 export const About = () => {
   return (
     <div className="w-full h-max flex flex-col mt-12 gap-4">
-      <h1 className="font-bold w-max text-[28px] before:w-[75%] before:h-[4px] before:rounded-full before:bg-[#67B5DC] relative before:absolute before:top-[100%]">About the Event</h1>
+      <h1 className="font-medium w-max text-[28px] before:w-[75%] before:h-[4px] before:rounded-full before:bg-[#67B5DC] relative before:absolute before:top-[100%]">About the Event</h1>
       <div className="text-[15px]">
         <p>
           “How to GIT Started” is a beginner-friendly webinar designed to show you the fastest, easiest, and most efficient way to connect your codebase to GitHub — directly from your favorite IDE like VS Code.
