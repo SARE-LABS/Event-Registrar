59b42487985684f3fcf0e113fe3b0ffe3a99f037
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-09 01:56:56 +0100

    feat: add ToastProps interface for toast notifications

--- src/Components/toast/Index.tsx
+++ src/Components/toast/Index.tsx
@@ -1,6 +1,39 @@
+import type { ToastProps } from "../../types/Toast"
+import Error from "../../assets/gifs/Error.gif"
+import Success from "../../assets/gifs/Success.gif"
 
-export const Toast = () => {
+export const Toast: React.FC<ToastProps> = ({ message, type }) => {
+  const baseStyle =
+    "fixed bottom-4 right-4 h-[100px] w-[400px] flex items-center gap-3 p-4 rounded-lg shadow-lg transition-opacity duration-300 bg-[#FFFFFF] z-50";
+
+  const typeStyles = [
+    {
+      type: "success",
+      style: "text-[#0FC99F]",
+      icon: <Success />,
+    },
+    {
+      type: "error",
+      style: "text-[#EF4444]",
+      icon: <Error />,
+    },
+  ];
+
+  const styleConfig = typeStyles.find((t) => t.type === type);
+
   return (
-    <div>Toast</div>
-  )
-}
+    <div className={`${baseStyle} ${styleConfig?.style}`}>
+      <div>
+        <h2 className="text-[12px] font-medium">
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </h2>
+        <p className="text-[15px] font-medium text-[#1F2937]">{message}</p>
+      </div>
+      {styleConfig?.icon && (
+        <div className="ml-4">
+          {styleConfig.icon}
+        </div>
+      )}
+    </div>
+  );
+};
